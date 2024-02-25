6f5872c79b2d93385c998ef0a60d77f57cc9b785
redde <redde@GIGAPC>
2024-02-25 19:59:24 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AssignmentOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fruitArray =
+            {
+                "melon",
+                "apple",
+                "guava",
+                "apricot",
+                "banana",
+                "grape",
+                "avocado" 
+            };
+
+            string[] fruitFilter = FilterFruitStartingWithA(fruitArray);
+
+            Console.WriteLine("Fruits that start with the letter 'a'");
+
+            foreach (string item in fruitFilter)
+
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.ReadKey();
+
+        }
+
+        static string[] FilterFruitStartingWithA(string[] fruit)
+        {
+            string[] filteredArray= new string[fruit.Length];
+            int index= 0;
+
+            foreach (string item in fruit)
+            {
+                if (item.StartsWith("a", StringComparison.OrdinalIgnoreCase)) filteredArray[index++] = item;
+            }
+
+          return filteredArray;
+
+        }
+    }
+
+}
